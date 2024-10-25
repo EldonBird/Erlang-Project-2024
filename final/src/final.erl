083a9956db3744eb6d294ee22e33405eb9d9c599
EldonBird <Birdheldon@gmail.com>
2024-10-25 15:40:12 -0600

    igyyvvyyygugyu

--- final/src/final.erl
+++ final/src/final.erl
@@ -1,6 +1,12 @@
 -module(final).
--export([start_mailbox_server/0, user_input/2, printout/1, mailbox_server/1, start_client_server/0, client_server/0]).
+-export([start_mailbox_server/0, user_input/2, printout/1, mailbox_server/1, start_client_server/0, client_server/0, start/0, user_interface/0, commander/1]).
 
+
+
+start() ->
+    start_mailbox_server(),
+    start_client_server(),
+    io:format("Client and Mailbox Server Started").
 
 start_mailbox_server() ->
     
@@ -15,8 +21,9 @@ mailbox_server(Mailbox) ->
             ClientPid ! Mailbox,
             mailbox_server(Mailbox);
         {ClientPid, Message, Request} when Request == 1 ->
-            Current = Mailbox ++ [Message],
-            ClientPid ! "Success",
+
+            Current = Mailbox ++ [{ClientPid, date(), time(), Message}],
+            ClientPid ! {"Message Sent"},
             mailbox_server(Current)
     end.
 
@@ -27,13 +34,9 @@ start_client_server() ->
 client_server() ->
 
     receive
-
         {UserPid, Message, Request} ->
-
-            Pid = global:whereis_name(mailbox_server_instance),   
-                             
+            Pid = global:whereis_name(mailbox_server_instance),               
             Pid ! {self(), Message, Request},
-
             receive
                 Serverout ->
                     UserPid ! {Serverout}
@@ -48,11 +51,33 @@ user_input(Message, Request) ->
     Pid ! {self(), Message, Request},
     
     receive
-        Responce->
-            printout(Responce)
+        Responce when Request == 0 ->
+            {Out} = Responce,
+            printout(Out);
+
+        Responce when Request == 1 ->
+            Responce
     end.
 
+user_interface() ->
+
+    io:format("What would you like to do? (message, mailbox)"),
+    Command = string:trim(io:get_line("")),
+    commander(Command),
+    user_interface().
 
 
-printout([])-> io:format("No mail Today");
-printout([H | T]) -> io:format(H), printout(T).
+commander("message") ->
+
+    io:format("What would you like to send?"),
+    Message = string:trim(io:get_line("")),
+    final:user_input(Message, 1);
+
+commander("mailbox") ->
+    final:user_input("na", 0).
+
+
+
+printout([]) -> ok;
+printout([{Key, Date, Time, Value} | T]) -> io:format("User: ~p, Date: ~p, Time: ~p, Message: ~s~n", [Key, Date, Time, Value]), printout(T).
+
